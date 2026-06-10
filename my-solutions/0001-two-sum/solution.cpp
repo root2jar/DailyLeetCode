e68a60b7ef6a2ad7b559a713a589857134e67449
root2jar <rutuja.r2007@gmail.com>
2026-06-10 10:04:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.7 MB (62.68%)

--- my-solutions/0001-two-sum/solution.cpp
+++ my-solutions/0001-two-sum/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
-            }
-        }
-        return{};
+  vector<int> twoSum(vector<int>& nums, int target){
+      unordered_map<int,int> seen_numbers;
+      for(int i=0;i<nums.size();i++){
+          int current_num=nums[i];
+          int complement=target-current_num;
+          if(seen_numbers.find(complement)!=seen_numbers.end()){
+              return {seen_numbers[complement],i};
+          }
+          seen_numbers[current_num]=i;
+      }
+      return{};
     }
 };  
 
