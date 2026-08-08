5bbb12c7bf66aa4fc0cc2d84516cda9501f97e2e
root2jar <rutuja.r2007@gmail.com>
2026-08-08 05:50:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (84.66%)

---
+++ my-solutions/0027-remove-element/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int w=0; int r; 
+for(r=0;r<nums.size();r++){
+if(nums[r]!=val){
+nums[w]=nums[r];
+w++; }
+}
+return w; 
+    }
+};
